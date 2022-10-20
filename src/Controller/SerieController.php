f7a7b92b3921700bf032f99b65adb48844f9c18c
cestaxelle <cardin.axelle@live.fr>
2022-10-20 10:23:56 +0200

    création de la BDD

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -12,14 +12,30 @@ class SerieController extends AbstractController
     #[Route('/', name: 'series_list')]
     public function list(): Response
     {
-        return $this->render('serie/index.html.twig');
+        $series = [
+            [
+                'id' => 1,
+                'title' => 'Corporate'
+            ],
+            [
+                'id' => 2,
+                'title' => 'The IT Crowd'
+            ]
+        ];
+
+        return $this->render('serie/index.html.twig', [
+            'series' => $series
+        ]);
     }
 
 //    je dois passer des requirements sinon à cause de /series/{id}, je ne passerai jamais dans /series/new
     #[Route('/{id}', name: 'series_detail', requirements: ['id' => '\d+'])]
-    public function detail(): Response
+    public function detail($id): Response
     {
-        return $this->render('serie/detail.html.twig');
+        // TODO: Récupérer la série à afficher en BDD
+        return $this->render('serie/detail.html.twig', [
+        'id' => $id
+        ]);
     }
 
     #[Route('/new', name: 'series_new')]
