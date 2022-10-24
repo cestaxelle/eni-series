b1e5f7c979b7a632f8782df02042d12a96651490
cestaxelle <cardin.axelle@live.fr>
2022-10-24 14:54:17 +0200

    commit formulaire ajout série

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Serie;
+use App\Form\SerieType;
 use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,8 +19,8 @@ class SerieController extends AbstractController
     {
         // je récupère les données dans le repo
         // premier paramètre du findBy = where, deuxième = order by
-//        $series = $serieRepository->findBy([], ['firstAirDate' => 'DESC', 'name' => 'ASC']);
-        $series = $serieRepository->findAllBetweenDates(new \DateTime('2019-01-01'), new \DateTime('2019-12-31'));
+        $series = $serieRepository->findBy([], ['firstAirDate' => 'DESC', 'name' => 'ASC']);
+//        $series = $serieRepository->findAllBetweenDates(new \DateTime('2019-01-01'), new \DateTime('2019-12-31'));
 
         return $this->render('serie/index.html.twig', [
             'series' => $series
@@ -44,8 +47,28 @@ class SerieController extends AbstractController
 //    }
 
     #[Route('/new', name: 'series_new')]
-    public function new(): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('serie/new.html.twig');
+        $serie = new Serie();
+        $serie->setDateCreated(new \DateTime()); // ou alors utiliser les LifeCycleCallbacks de Doctrine
+        $serieForm = $this->createForm(SerieType::class, $serie);
+
+        $serieForm->handleRequest($request);    // récupère ce qu'il y a dans le form et le met dans $serie
+
+        // je vérifie si l'utilisateur est en train d'envoyer le formulaire ou s'il faut juste l'afficher
+        if ($serieForm->isSubmitted()) {
+            // s'il est en train d'envoyer le form, j'enregistre la nouvelle série en BDD
+            $em->persist($serie);
+            $em->flush();
+
+            $this->addFlash('success', 'La série a bien été créée !');
+
+            // et je redirige l'ut vers la liste des séries
+            return $this->redirectToRoute('series_list');
+        }
+
+        return $this->render('serie/new.html.twig', [
+            'serieForm' => $serieForm->createView()
+        ]);
     }
 }
