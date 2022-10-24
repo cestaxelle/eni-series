86c42df58d7d2b89a80fd5e66c74c0c602bb5b1e
cestaxelle <cardin.axelle@live.fr>
2022-10-24 14:54:25 +0200

    commit formulaire ajout série

---
+++ src/Form/SerieType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Serie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SerieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('overview', TextareaType::class, [
+                'label' => 'Résumé'
+            ])
+            ->add('status')
+            ->add('vote', NumberType::class, [
+                'html5' => true
+            ])
+            ->add('popularity', NumberType::class, [
+                'html5' => true
+            ])
+            ->add('genres')
+            ->add('firstAirDate', DateType::class, [
+                    'widget' => 'single_text'
+            ])
+            ->add('lastAirDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('backdrop', FileType::class)
+            ->add('poster', FileType::class)
+            ->add('tmdbId')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Serie::class,
+        ]);
+    }
+}
